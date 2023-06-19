a5f3d63bbf4e245bd737ba2e07498750e1318813
Maria Cosmen <mcosme00@gmail.com>
2023-06-19 23:14:41 +0900

    Refactor the code for Steps

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,21 @@
 import Step from "./Step";
 
 const Sidebar = () => {
+  const steps = [
+    {id: 1, number: 1, step: "step 1", title: 'Your info'},
+    {id: 2, number: 2, step: "step 2", title: 'Select plan'},
+    {id: 3, number: 3, step: "step 3", title: 'Add-ons'},
+    {id: 4, number: 4, step: "step 4", title: 'Summary'},
+  ]
 
+  const renderSteps = steps.map((step) => {
+    return <Step key={step.id} number={step.number} step={step.step} title={step.title} />
+  })
 
   return (
     <nav className="relative w-full h-2/6 md:h-full md:w-md-sidebar-width md:rounded-xl lg:w-lg-sidebar-width p-8
     flex justify-center items-start md:flex-col md:justify-start">
-      <Step id='1' number='1' step='step1' title='Your info' selected/>
-      <Step id='2' number='2' step='step2' title='Select plan'/>
-      <Step id='3' number='3' step='step3' title='Add-ons'/>
-      <Step id='4' number='4' step='step4' title='Summary'/>
+      {renderSteps}
     </nav>
   )
 }
